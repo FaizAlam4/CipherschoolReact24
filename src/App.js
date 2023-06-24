983b20ddf0325b01fa6f4c97d43509fddeb8e3d1
Faiz <faizalam4e@gmail.com>
2023-06-24 19:47:50 +0530

    cipherschool's 24th june assignment

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './Header';
+import Form from './Form';
+import Output from './Output';
+
+function App() {
+  return (
+   <div>
+    <Header/>
+    <div className='app'>
+    
+
+    <Form/>
+    <Output/>
+    </div>
+    
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div>
+      <div className="Apph">
+   <li className="ap">Home</li>
+   <li className="ap">About</li>
+   <li className="ap">Contact</li>
+   <li className="ap">Register</li>
+    </div>
+    </div>
+  )
+}
+
+export default Header
